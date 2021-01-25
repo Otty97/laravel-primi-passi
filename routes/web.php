17343f7fdef86303f2471753a552338d0106350d
Unknown <taikothman1997@gmail.com>
2021-01-25 22:33:58 +0100

    Vue Dischi

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/homepage', function () {
+    return view('homepage');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/dischi', function () {
+    return view('dischi');
+});
+
+Route::get('/dischi','DischiController@index');
+
